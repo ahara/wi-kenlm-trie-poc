44ef7974df311305148f83db7622b3d0c21c1b93
Adam Harasimowicz <adam.ha@webinterpret.com>
2017-06-13 23:05:33 +0100

    Add retrieving element from trie

--- lm/kenlm_ttable_main.cc
+++ lm/kenlm_ttable_main.cc
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <string>
+
 #include "lm/model.hh"
+#include "lm/virtual_interface.hh"
 
 int main() {
-  printf("TEST\n");
-
   lm::ngram::Config config;
   config.load_method=util::LAZY;
-  lm::ngram::TrieModel* modelPtr=new lm::ngram::TrieModel(NULL,config);
+  lm::ngram::TrieModel* model = new lm::ngram::TrieModel("/home/adam/Tools/kenlm/lm/test.arpa",config);
+
+  lm::ngram::State out_state;
+  lm::WordIndex context[1];
+  std::string key = "also";
+  context[0] = model->GetVocabulary().Index(key.c_str());
+  model->GetState(context, context + 1, out_state);
+
+  printf("%u %f %u\n", out_state.length, out_state.backoff[0], out_state.words[0]);
 
   return 0;
 }
